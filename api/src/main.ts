df24e07e1676c416109b9a16e37fec2125bc4f55
cardiox12 <com.bellavi@tutanota.com>
2025-05-31 23:57:38 +0200

    feat: add user entity

--- api/src/main.ts
+++ api/src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: "http://localhost:8000"
+    origin: 'http://localhost:8000',
   });
   await app.listen(process.env.PORT ?? 3000);
 }
