66149fc95049b4f0df494948152b7f43ebcd04a7
cardiox12 <com.bellavi@tutanota.com>
2025-05-31 15:12:54 +0200

    feat: add configuration

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DBModule } from './db/db.module';
+import { AppConfigModule } from './app-config/app-config.module';
 
 @Module({
-  imports: [],
+  imports: [DBModule, AppConfigModule],
   controllers: [AppController],
   providers: [AppService],
 })
