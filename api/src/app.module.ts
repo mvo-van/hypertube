13f00414f73a68e65c384a964e792ac371cfbbfa
cardiox12 <com.bellavi@tutanota.com>
2025-05-31 16:01:57 +0200

    feat: add health check to docker

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DBModule } from './db/db.module';
 import { AppConfigModule } from './app-config/app-config.module';
+import { HealthModule } from './health/health.module';
 
 @Module({
-  imports: [DBModule, AppConfigModule],
+  imports: [DBModule, AppConfigModule, HealthModule],
   controllers: [AppController],
   providers: [AppService],
 })
