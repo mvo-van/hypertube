01c6d4820e7aa17986969427ea9b7981aa756566
cardiox12 <com.bellavi@tutanota.com>
2025-05-31 23:57:38 +0200

    feat: add user entity

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { DBModule } from './db/db.module';
 import { AppConfigModule } from './app-config/app-config.module';
 import { HealthModule } from './health/health.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [DBModule, AppConfigModule, HealthModule],
+  imports: [DBModule, AppConfigModule, HealthModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- api/src/app.service.ts
+++ api/src/app.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@nestjs/common';
-import {ConfigService} from "@nestjs/config";
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
   constructor(private readonly configService: ConfigService) {}
 
   getHello(): string {
-    const POSTGRES_USER = this.configService.get<string>("POSTGRES_USER");
+    const POSTGRES_USER = this.configService.get<string>('POSTGRES_USER');
     console.log(`User: ${POSTGRES_USER}`);
     return 'Hello World!';
   }
