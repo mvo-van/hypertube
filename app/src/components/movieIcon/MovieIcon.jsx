9866040ec366ac1826492c5a38232f8d043ab3c1
mainhi <mainhivvt@gmail.com>
2025-05-31 23:12:58 +0200

    add feed

--- app/src/components/movieIcon/MovieIcon.jsx
+++ app/src/components/movieIcon/MovieIcon.jsx
@@ -1,12 +1,21 @@
 import style from "./MovieIcon.module.css";
 import imageSee from "../../assets/images/see.png"
-function MovieIcon({ movie }) {
+import { useNavigate } from "react-router";
+
+
+function MovieIcon({ movie, color=0 }) {
+    const navigate = useNavigate()
+
+    const onClickHandlerMovie = () => {
+            navigate(`/movie/${movie.id}`);
+       }
+    
   console.log(movie)
   return (
-    <div className={style.movieIcon}>
-      {movie.see && <div className={style.see}><img className={style.notif} src={imageSee}/></div>}
+    <div className={`${style.movieIcon} ${style[`movieIcon__color_${color}`]}`} onClick={onClickHandlerMovie}>
+      {movie.see && <div className={`${style.see} ${style[`see__color_${color}`]}`}><img className={style.notif} src={imageSee}/></div>}
       <img className={style.poster} src={movie.urlImg}/>
-        <p className={style.movieName}>{movie.name} lkfjslqkjf skjf lkjqlkjf lkj mlskqjf slkjflksjsf lkjlsdkjklfj qlksjfldskqj qlkjsdflkjq</p>
+        <p className={style.movieName}>{movie.name}</p>
     </div>
   );
 }
