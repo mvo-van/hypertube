7f3f3de97db6195211a0fbd67b8a762dadc420de
mainhi <mainhivvt@gmail.com>
2025-06-02 14:46:14 +0200

    add : filter layout

--- app/src/components/form/Form.jsx
+++ app/src/components/form/Form.jsx
@@ -1,21 +1,26 @@
 import Button from "../button/Button";
 import styles from "./Form.module.css";
 
-function Form({ className, label, onSubmit, disabled = false, children, color="blue" }) {
+function Form({ className, label, onSubmit, disabled = false, children, color="blue", direction="column"}) {
     const formClasses = `${styles.form} ${className}`;
 
     return (
         <form className={formClasses} onSubmit={onSubmit}>
-            {children}
-            <Button 
-                type="submit"
-                variant="validation"
-                className={styles['form__submit']}
-                disabled={disabled}
-                color={color}
-            >
-                {label}
-            </Button>
+            <div className={styles[`form__${direction}`]}>
+                {children}
+            </div>
+            <div className={styles.div__submit}>
+                <Button 
+                    type="submit"
+                    variant="validation"
+                    className={styles['form__submit']}
+                    disabled={disabled}
+                    color={color}
+                >
+                    {label}
+                </Button>
+            </div>
+
         </form>
     );
 }
