03a8ed14217b7365ea1008233f04e33ab9bcdf58
cardiox12 <com.bellavi@tutanota.com>
2025-06-29 21:03:26 +0200

    WIP: users

---
+++ api/src/users/constants.ts
@@ -0,0 +1,10 @@
+export const MIN_LENGTH_PSEUDO: number = 3;
+export const MAX_LENGTH_PSEUDO: number = 20;
+export const MIN_LENGTH_FIRST_NAME: number = 2;
+export const MAX_LENGTH_FIRST_NAME: number = 20;
+export const MIN_LENGTH_LAST_NAME: number = 2;
+export const MAX_LENGTH_LAST_NAME: number = 20;
+
+export const MAX_LENGTH_PASSWORD: number = 72;
+export const MIN_LENGTH_PASSWORD: number = 12;
+export const MAX_LENGTH_PICTURE_URL: number = 255;
