ae7498a83543a299d47d10590db43ec807224657
Mai-nhi VO VAN TAO <mvo-van-@f2r4s23.clusters.42paris.fr>
2025-07-02 15:42:05 +0200

    user

---
+++ app/src/components/comments/Comments.jsx
@@ -0,0 +1,20 @@
+import style from "./Comments.module.css";
+import { useNavigate } from "react-router";
+
+
+function Comments({ comments, color=0 }) {
+    const navigate = useNavigate()
+
+    const onClickHandlerUser = () => {
+            navigate(`/movie/${comments.movieId}`);
+       }
+    
+  console.log(comments)
+  return (
+    <div className={style.Allcomments} onClick={onClickHandlerUser}>
+      {comments.length && <div>Commantaires :</div>}
+    </div>
+  );
+}
+
+export default Comments;
