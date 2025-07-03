fccb5851ee85fc2051a5d7eb6c7d77bfacce0a81
cardiox12 <com.bellavi@tutanota.com>
2025-07-03 23:54:05 +0200

    feat: exclude password from response

--- api/src/users/users.controller.ts
+++ api/src/users/users.controller.ts
@@ -5,7 +5,7 @@ import {
   Body,
   Patch,
   Param,
-  Delete,
+  Delete, UseInterceptors, ClassSerializerInterceptor,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
@@ -13,13 +13,14 @@ import { UpdateUserDto } from './dto/update-user.dto';
 import {UserResponseDto} from "./dto/user-response.dto";
 
 @Controller('users')
+@UseInterceptors(ClassSerializerInterceptor)
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
   async create(@Body() createUserDto: CreateUserDto): Promise<UserResponseDto> {
-    console.info(`[CONTROLLER] Creating user: ${createUserDto}`);
-    return await this.usersService.create(createUserDto);
+    const createdUser = await this.usersService.create(createUserDto);
+    return new UserResponseDto(createdUser);
   }
 
   @Get()
