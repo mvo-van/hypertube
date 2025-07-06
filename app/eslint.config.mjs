8823ac93cd65ac83e74de364c8d3b07b6a645a6a
cardiox12 <com.bellavi@tutanota.com>
2025-07-06 15:20:26 +0200

    feat: lint react app

---
+++ app/eslint.config.mjs
@@ -0,0 +1,20 @@
+import js from "@eslint/js";
+import globals from "globals";
+import pluginReact from "eslint-plugin-react";
+
+export default [
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    languageOptions: {
+      globals: globals.browser,
+    },
+    plugins: {
+      js,
+      react: pluginReact,
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...pluginReact.configs.recommended.rules,
+    },
+  },
+];
