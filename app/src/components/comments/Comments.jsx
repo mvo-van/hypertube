6c723cfea2d8f42c26545b9fe7ef9b9e7b70de99
mvo-van <mainhivvt@gmail.com>
2025-07-13 10:57:18 +0200

    css comment

--- app/src/components/comments/Comments.jsx
+++ app/src/components/comments/Comments.jsx
@@ -1,18 +1,15 @@
+import CommentDiv from "../commentDiv/CommentDiv";
 import style from "./Comments.module.css";
 import { useNavigate } from "react-router";
 
 
-function Comments({ comments, color=0 }) {
-  const navigate = useNavigate()
+function Comments({ comments, color=0, movieIcon=true }) {
 
-  const onClickHandlerUser = () => {
-    navigate(`/movie/${comments.movieId}`);
-  }
-    
   console.log(comments)
   return (
-    <div className={style.Allcomments} onClick={onClickHandlerUser}>
-      {comments.length && <div>Commantaires :</div>}
+    <div className={style.Allcomments}>
+      {comments.length && <div className={`${style.title} ${style[`title__color_${color}`]}`}>Commantaires :</div>}
+      {comments.map((comment, index) => <CommentDiv key={comment.commentId} comment={comment} color={(color+index)%12} movieIcon={movieIcon}/>)}
     </div>
   );
 }
