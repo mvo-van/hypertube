f34f1c3a9c1959dda17caf7943901f627dea80b5
cardiox12 <com.bellavi@tutanota.com>
2025-09-12 22:05:32 +0200

    feat: add auto-generated docs with swagger

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ClassSerializerInterceptor } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,6 +10,17 @@ async function bootstrap() {
     origin: 'http://localhost:8000',
   });
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+
+
+  const config = new DocumentBuilder()
+    .setTitle("Hypertube API")
+    .setDescription("Routes list")
+    .setVersion("1.0")
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("docs", app, document);
+  
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
