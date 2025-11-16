3e104cb0416d8c95f3f079df6202ffac643476c4
fullife32 <eithan.assouline6@gmail.com>
2025-11-16 18:48:08 +0100

    pause axios for now

--- app/src/pages/signup/Signup.jsx
+++ app/src/pages/signup/Signup.jsx
@@ -18,7 +18,7 @@ import {
 	ERROR_INVALID_PASSWORD,
 } from "../../common/messages";
 import { useNavigate } from "react-router";
-import axios from "axios";
+// import axios from "axios";
 
 function Signup() {
 	const [email, setEmail] = useState("");
