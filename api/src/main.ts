401d23b227e496c783e07080c9e90398bde6d48f
cardiox12 <com.bellavi@tutanota.com>
2025-11-25 11:47:38 +0100

    fix: await data bootstrap

--- api/src/main.ts
+++ api/src/main.ts
@@ -34,7 +34,7 @@ async function bootstrap() {
   SwaggerModule.setup('docs', app, document);
 
   const mocker = app.get(MockDataService);
-  mocker.bootstrap();
+  await mocker.bootstrap();
 
   await app.listen(process.env.PORT ?? 3000);
 }
