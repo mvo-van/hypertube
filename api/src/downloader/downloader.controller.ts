204c00a6d279f00b18efd365690b4fb07c18d05a
cardiox12 <com.bellavi@tutanota.com>
2026-02-17 20:47:16 +0100

    feat: add jackett config

--- api/src/downloader/downloader.controller.ts
+++ api/src/downloader/downloader.controller.ts
@@ -1,17 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { DownloaderService } from './downloader.service';
 
 // TODO: TESTING ONLY, remove after flight
-@Controller('downloader')
+@Controller('download')
 export class DownloaderController {
   constructor(private readonly downloaderService: DownloaderService) {}
 
-  @Get()
-  download() {
+  @Get(':imdb')
+  download(@Param('imdb') imdb: string) {
     // 1. Search a magnet on yts and popcorn
     // 2. Start the download with torrent-stream node package
     //  2.a. Start the download in a specific folder, named with the imdb ID
     // 3. Notify the streaming service that the download has started
-    this.downloaderService.donwload();
+    this.downloaderService.donwload(imdb);
   }
 }
