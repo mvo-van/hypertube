c050079e96431046e971712f543371007a035ccc
mvo-van <mainhivvt@gmail.com>
2026-03-01 00:40:41 +0100

    fix video player

--- app/src/pages/moviePage/VideoPlayer.jsx
+++ app/src/pages/moviePage/VideoPlayer.jsx
@@ -1,19 +1,23 @@
 import React, { useEffect, useState } from "react";
 import { api } from "../../common/api";
+import style from "./VideoPlayer.module.css"
 
-export const VideoPlayer = (imdbID) => {
+export const VideoPlayer = ({ imdbID }) => {
   // const thumbnail =
   //   "https://disney.images.edge.bamgrid.com/ripcut-delivery/v2/variant/disney/4a55d5e6-8e3b-4cab-a34c-e8dbe51b5cc2/compose?aspectRatio=1.78&format=webp&width=1200";
   const [subtitles, setSubtitles] = useState([]);
-
+  console.log(imdbID)
   const movie = `http://localhost:3000/stream/imdb/${imdbID}`;
 
   const getSubtitles = async () => {
     try {
-      const res = await api.get(`http://localhost:3000/stream/${imdbID}/subs`);
+      if (imdbID) {
+        const res = await api.get(`http://localhost:3000/stream/${imdbID}/subs`);
 
-      setSubtitles(res.data.subtitles);
-      console.log(res);
+        setSubtitles(res.data.subtitles);
+        console.log(res);
+      }
+
     } catch (e) {
       console.log(e);
     }
@@ -21,15 +25,14 @@ export const VideoPlayer = (imdbID) => {
 
   useEffect(() => {
     getSubtitles();
-  }, [])
+  }, [imdbID])
 
   return (
-    <video
+    <video className={style.videoPlayer}
       src={movie}
       controls
       preload="auto"
       autoPlay
-      width="800"
       crossOrigin="anonymous"
     >
       {subtitles.reverse().map((e, index) =>
