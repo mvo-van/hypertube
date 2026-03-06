4e36119072c2b58d72477adaab09187fa98595b8
tbellavia <bbellavi@student.42.fr>
2026-03-06 14:19:06 +0100

    fix: problem on streaming

--- api/src/stream/stream.controller.ts
+++ api/src/stream/stream.controller.ts
@@ -28,6 +28,7 @@ export class StreamController {
   ) { }
 
   @Get('/imdb/:imdbID')
+  @Public()
   async streamFromImdb(@Req() req: Request, @Res() res: Response, @Param('imdbID') imdbID: string) {
     this.logger.log(`[${imdbID}]: retrieving filepath`);
     let filepath = await this.mediaFileService.getMediaFilePath(imdbID);
